f5fd3b238384ed80fa3e970438e977f31dbfc358
terrahsa@gmail.com <118025389+AdrianB378@users.noreply.github.com>
2023-12-29 19:43:20 -0300

    Se agrego el proyecto

---
+++ promedioNotasOk/src/main/java/com/todocode/promedioNotasOk/PromedioNotasOkApplication.java
@@ -0,0 +1,13 @@
+package com.todocode.promedioNotasOk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromedioNotasOkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromedioNotasOkApplication.class, args);
+	}
+
+}
---
+++ promedioNotasOk/src/test/java/com/todocode/promedioNotasOk/PromedioNotasOkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todocode.promedioNotasOk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromedioNotasOkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
